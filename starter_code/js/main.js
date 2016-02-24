3904d5ddfde07e498ce338aca3dfd2f751ef62df
HarveenS <ronith19>
2016-02-24 13:13:09 +1100

    i am adding city names with capital letters. but when i change more than 2 cities, it stops updating, till i refresh the page.

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
 		//get the value of the #city-type input and correct for capitalization
 		var city = $('#city-type').val();
 		// if the user inputs New York City / NYC / New York change the body class to 'nyc'
-		if (city === "New York City" || city === "New York" || city === "NYC") {
+		if (city === "New York City" || city === "New York" || city === "NYC" || city === "nyc" || city === "new york city" || city === "new york") {
 			$('body').addClass('nyc');
 		}
 		// if the user inputs SF/San Francisco/Bay Area change the body class to 'sf'
-		else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+		else if (city === "San Francisco" || city === "SF" || city === "Bay Area" || city === "san francisco" || city === "bay area") {
 			$('body').addClass('sf');
 
 		}
@@ -20,12 +20,12 @@ $(document).ready(function () {
 
 		}
 		// if the user inputs Los Angeles/LA/LAX change the body class to 'la'
-		else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+		else if (city === "Los Angeles" || city === "LA" || city === "LAX" || city === "los angeles") {
 			$('body').addClass('la');
 
 		}
 		// if the user inputs Sydney/SYD change the body class to 'sydney'
-		else if (city === "Sydney" || city === "SYD") {
+		else if (city === "Sydney" || city === "SYD" || city === "sydney") {
 			$('body').addClass('sydney');
 
 		}
